6198235246327a741c15e24323d0ff5bbc285800
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-11 19:39:41 -0700

    working!

---
+++ app.js
@@ -0,0 +1,17 @@
+import add from './add.js';
+
+// reference elements
+const xInput = document.getElementById('x');
+const yInput = document.getElementById('y');
+const sumResult = document.getElementById('sum-result');
+
+//wire up events
+xInput.oninput = showSum;
+yInput.oninput = showSum;
+
+function showSum() {
+    const x = Number(xInput.value);
+    const y = Number(yInput.value);
+
+    sumResult.textContent = add(x, y);
+}
